f505ac0b3da200c95962cf22839b6ff9b9922f7f
George Koukoudis <grkouk@hotmail.com>
2022-12-26 13:34:26 +0200

    Initial Commit

---
+++ erp.client/erp_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  erp_clientApp.swift
+//  erp.client
+//
+//  Created by George Koukoudis on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct erp_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
